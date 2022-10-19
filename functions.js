e866d682d5a8bda0106a4c26efb25bc9dc55f9c7
Kristina Ledinski <komarcica@gmail.com>
2022-10-19 11:22:19 +0200

    js hide/show cards, check rating

--- functions.js
+++ functions.js
@@ -1,7 +1,15 @@
+var x;
+
 function storeValue(rating) {
     x = rating;
 }
 function submit() {
-    document.getElementById("card1").style.display = "none";
-    document.getElementById("card2").style.display = "flex";
-  }
+    if (x != null) {
+        document.getElementById("card1").style.display = "none";
+        document.getElementById("card2").style.display = "flex";
+        document.getElementById("rating").innerHTML = "&nbsp;" + x +  "&nbsp;";
+    }
+    else {
+        document.getElementById("alert").innerHTML ="Please select a rating!";
+    }
+}
