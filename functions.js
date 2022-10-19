d9368cae7cc6abed449ae16dcd94a4b89be6ba27
Kristina Ledinski <komarcica@gmail.com>
2022-10-19 10:59:42 +0200

    js: store rating and switch cards

---
+++ functions.js
@@ -0,0 +1,7 @@
+function storeValue(rating) {
+    x = rating;
+}
+function submit() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "flex";
+  }
